42c34bb75bb6cbb532307c7c9f3f8d8c2cb7981b
doubleface <christophe@cozycloud.cc>
2017-12-10 17:14:33 +0100

    fix: adaptation to new oui.sncf website

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function logIn (fields) {
   // Directly post credentials
   log('info', 'Logging in in SNCF.')
   return rq({
-    uri: 'https://espace-client.voyages-sncf.com/espaceclient/authentication/flowSignIn',
+    uri: 'https://www.oui.sncf/espaceclient/authentication/flowSignIn',
     method: 'POST',
     form: {
       login: fields.login,
@@ -62,7 +62,7 @@ function getPastOrderPage () {
   })
 
   log('info', 'Download past orders HTML page...')
-  return rq('https://espace-client.voyages-sncf.com/espaceclient/ordersconsultation/showOrdersForAjaxRequest?pastOrder=true&pageToLoad=1')
+  return rq('https://www.oui.sncf/espaceclient/ordersconsultation/showOrdersForAjaxRequest?pastOrder=true&pageToLoad=1')
 }
 
 function getCurrentOrders () {
@@ -72,47 +72,57 @@ function getCurrentOrders () {
   })
 
   log('info', 'Download current orders ...')
-  return rq('https://secure.voyages-sncf.com/espaceclient/ordersconsultation/getCurrentUserOrders')
+  return rq('https://www.oui.sncf/espaceclient/ordersconsultation/getCurrentUserOrders')
   .then(body => {
     return bluebird.mapSeries(body.trainOrderList, trainOrder => {
       const code = Object.keys(trainOrder.pnrsAndReceipt).pop()
-      return rq(`https://www.voyages-sncf.com/vsa/api/order/fr_FR/${trainOrder.owner}/${code}?source=vsa`)
-      .then(body => {
-        let creationDate = body.order.trainFolders[code].creationDate
-        creationDate = creationDate
-          .replace(/-/g, '')
-          .replace(/T/g, '')
-          .replace(/:/g, '')
-        creationDate = creationDate.substr(0, creationDate.length - 2)
-        const date = new Date(trainOrder.outwardDate)
-        return {
-          date,
-          amount: trainOrder.amount,
-          vendor: 'VOYAGES SNCF',
-          type: 'transport',
-          content: `${trainOrder.originLabel}/${trainOrder.destinationLabel} - ${code}`,
-          fileurl: 'https://ebillet.voyages-sncf.com/ticketingServices/public/e-ticket/',
-          filename: getFileName(moment(date), '_ebillet'),
-          requestOptions: {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json, text/plain, */*'
-            },
-            body: {
-              lang: 'FR',
-              pnrRefs: [
-                {
-                  pnrLocator: code,
-                  creationDate: creationDate,
-                  passengerName: trainOrder.owner
-                }
-              ],
-              market: 'VSC',
-              caller: 'VSA_FR'
+      const date = new Date(trainOrder.outwardDate)
+      let entry = {
+        date,
+        amount: trainOrder.amount,
+        vendor: 'VOYAGES SNCF',
+        type: 'transport',
+        content: `${trainOrder.originLabel}/${trainOrder.destinationLabel} - ${code}`
+      }
+
+      if (trainOrder.deliveryMode === 'EADN') { // délivré par courrier
+        return entry
+      } else {
+        return rq(`https://www.oui.sncf/vsa/api/order/fr_FR/${trainOrder.owner}/${code}?source=vsa`)
+        .then(body => {
+          let creationDate = body.order.trainFolders[code].creationDate
+          creationDate = creationDate
+            .replace(/-/g, '')
+            .replace(/T/g, '')
+            .replace(/:/g, '')
+          creationDate = creationDate.substr(0, creationDate.length - 2)
+
+          Object.assign(entry, {
+            fileurl: 'https://ebillet.oui.sncf/ticketingServices/public/e-ticket/',
+            filename: getFileName(moment(date), '_ebillet'),
+            requestOptions: {
+              method: 'POST',
+              headers: {
+                Accept: 'application/json, text/plain, */*'
+              },
+              body: {
+                lang: 'FR',
+                pnrRefs: [
+                  {
+                    pnrLocator: code,
+                    creationDate: creationDate,
+                    passengerName: trainOrder.owner
+                  }
+                ],
+                market: 'VSC',
+                caller: 'VSA_FR'
+              }
             }
-          }
-        }
-      })
+          })
+
+          return entry
+        })
+      }
     })
   })
 }
@@ -120,7 +130,7 @@ function getCurrentOrders () {
 function parseOrderPage ($) {
   // Parse the orders page
   const result = []
-  const $rows = $('.commande')
+  const $rows = $('.order')
   $rows.each(function eachRow () {
     const $row = $(this)
     const orderInformations = parseOrderRow($, $row)
@@ -146,39 +156,28 @@ function parseOrderPage ($) {
 }
 
 function parseOrderRow ($, $row) {
-  const reference = $row.find('.commande__detail div:nth-child(1) .texte--important')
-                        .eq(0)
-                        .text()
-                        .trim()
-  const label = $row.find('.commande__haut .texte--insecable')
+  const reference = $row.find(`.order__detail [data-auto=ccl_orders_travel_number]`).text().trim()
+  const label = $row.find('.order__top .texte--insecable')
                     .map(function mapRow () {
                       return $(this).text().trim()
                     })
                     .get()
                     .join('/')
-  const date = $row.find('.commande__detail div:nth-child(2) .texte--important')
-                   .eq(0)
-                   .text()
-                   .trim()
-  const amount = $row.find('.commande__detail div:nth-child(3) .texte--important')
+  const date = $row.find('.order__detail div:nth-child(2) .texte--important').eq(0).text().trim()
+  const amount = $row.find('.order__detail div:nth-child(3) .texte--important')
                      .eq(0)
                      .text()
                      .trim()
                      .replace(' €', '')
 
-  const $link = $row.find('.commande__bas a')
-  // Boolean, the order is not always a travel (could be a discount card...)
-  const isTravel = $link.text().trim().indexOf('voyage') !== -1
-
   const result = {
     reference,
     label,
     date,
-    amount,
-    isTravel
+    amount
   }
 
-  const $filelink = $row.find('.commande__detail a:not([target=_blank])')
+  const $filelink = $row.find('.order__detail a:not([target=_blank])')
   if ($filelink.length > 0) {
     result.pdfurl = $filelink.eq(0).attr('href')
   }
