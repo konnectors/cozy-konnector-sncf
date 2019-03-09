8628b71ed317b26e2d7d8a060b58b458c0a1b683
doubleface <christophe@cozycloud.cc>
2019-03-09 08:51:14 +0100

    fix: do not follow any redirect for captcha response

--- src/index.js
+++ src/index.js
@@ -43,7 +43,8 @@ class SncfConnector extends CookieKonnector {
               Referer:
                 'https://www.oui.sncf/customer/api/clients/customer/authentication'
             },
-            followRedirect: false
+            followRedirect: false,
+            followAllRedirects: false
           }
         )
         try {
