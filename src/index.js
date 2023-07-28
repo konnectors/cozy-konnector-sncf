2d45774533925ef55a2f55949fe97697eebbdad7
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2023-07-28 19:04:39 +0200

    feat: Fetch and save bills

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import { ContentScript } from 'cozy-clisk/dist/contentscript'
 import Minilog from '@cozy/minilog'
 import waitFor, { TimeoutError } from 'p-wait-for'
+import { format } from 'date-fns'
 const log = Minilog('ContentScript')
 Minilog.enable('sncfCCC')
 
 const baseUrl = 'https://sncf-connect.com'
 const preLoginPage = 'https://www.sncf-connect.com/app/account'
-const loginPage =
-  'https://monidentifiant.sncf/login?scope=openid%20profile%20email&response_type=code&client_id=CCL_01002&redirect_uri=https:%2F%2Fwww.sncf-connect.com%2Fbff%2Fapi%2Fv1%2Fauthenticated-redirect'
+
+let jsonTrips = []
+// The override here is needed to intercept XHR requests made during the navigation
+// The website respond with an XHR containing a JSON with all trips information, avoiding a lot of navigation.
+var proxiedSend = window.XMLHttpRequest.prototype.send
+
+window.XMLHttpRequest.prototype.send = function () {
+  var originalResponse = this
+  originalResponse.addEventListener('readystatechange', function () {
+    if (originalResponse.readyState === 4) {
+      if (originalResponse.responseURL.includes('/bff/api/v1/trips')) {
+        jsonTrips.push(JSON.parse(originalResponse.response))
+      }
+    }
+  })
+  return proxiedSend.apply(this, [].slice.call(arguments))
+}
 
 class SncfContentScript extends ContentScript {
   async navigateToLoginForm() {
@@ -149,6 +165,18 @@ class SncfContentScript extends ContentScript {
     if (this.store.userCredentials) {
       await this.saveCredentials(this.store.userCredentials)
     }
+    await this.saveIdentity(this.store.userIdentity)
+    await this.navigateToBillsPage()
+    await this.runInWorkerUntilTrue({
+      method: 'waitForInterception'
+    })
+    const bills = await this.runInWorker('getBills')
+    await this.saveBills(bills, {
+      context,
+      fileIdAttributes: ['vendorRef', 'filename'],
+      contentType: 'application/pdf',
+      qualificationLabel: 'transport_invoice'
+    })
   }
 
   async navigateToPersonnalInfos() {
@@ -156,16 +184,24 @@ class SncfContentScript extends ContentScript {
     await this.runInWorker('findAndClickInfosButton')
   }
 
+  async navigateToBillsPage() {
+    this.log('info', 'navigateToBillsPage starts')
+    await this.clickAndWait('a[href="/app/trips"]', '#nav-tab-1')
+    await this.clickAndWait('#nav-tab-1', 'li[data-test="trip"]')
+  }
+
   async waitForOtpCode() {
     this.log('info', 'waitForOtpCode starts')
     await waitFor(
       () => {
-        if (
-          document.querySelector(
-            'button[data-test="account-disconnect-button"]'
-          )
+        const continueButton = document.querySelector('#accessAccount')
+        const logoutButton = document.querySelector(
+          'button[data-test="account-disconnect-button"]'
         )
-          return true
+        if (continueButton) {
+          continueButton.click()
+        }
+        if (logoutButton) return true
         else return false
       },
       {
@@ -213,7 +249,7 @@ class SncfContentScript extends ContentScript {
       .querySelectorAll('[data-cy-element="identity-summary-label"]')
     const fullName = nameBirthPhoneElements[0].textContent
     const [givenName, familyName] = fullName.split(' ')
-    // split is on 'le ' because the found string starts with "Né le"
+    // we split on 'le ' because the found string starts with "Né le"
     const birthDate = nameBirthPhoneElements[1].textContent.split('le ')[1]
     const phoneNumber = nameBirthPhoneElements[2].textContent
     // Here it's needed to fetch the 'a' element before trying to reach the wanted element
@@ -241,6 +277,200 @@ class SncfContentScript extends ContentScript {
     }
     await this.sendToPilot({ userIdentity })
   }
+
+  async waitForInterception() {
+    this.log('info', 'waitForInterception starts')
+    await waitFor(
+      () => {
+        // jsonTrips.length must be 2 as the website load twice the data, once when you reach the bills page
+        // and once more when you click on the passedTrips button
+        if (jsonTrips.length === 2) return true
+        else return false
+      },
+      {
+        interval: 1000,
+        timeout: {
+          milliseconds: 30000,
+          message: new TimeoutError(
+            'waitForInterception timed out after 30000ms'
+          )
+        }
+      }
+    )
+    return true
+  }
+
+  async getBills() {
+    this.log('info', 'getBills starts')
+    const foundBills = this.findPassedTrips()
+    return foundBills
+  }
+
+  findPassedTrips() {
+    this.log('info', 'findPassedTrips starts')
+    // we're poping the array to get just the last entry as it is the json for passed trips
+    const foudTripsInfos = jsonTrips.pop()
+    const foundTrips = foudTripsInfos.response.passedTrips
+    const allBills = []
+    for (const oneTrip of foundTrips) {
+      if (oneTrip.trip.isRoundTrip) {
+        this.log('debug', 'This trip is a round trip')
+        const bills = this.handleRoundTrip(oneTrip)
+        for (const oneBill of bills) {
+          if (oneBill === null) {
+            this.log('info', 'No pdf attached to this bill, jumping it')
+            continue
+          }
+          allBills.push(oneBill)
+        }
+      } else {
+        this.log('debug', 'This trip is a one way trip')
+        const bill = this.handleSingleTrip(oneTrip)
+        if (bill === null) {
+          this.log('info', 'No pdf attached to this bill, jumping it')
+          continue
+        }
+        allBills.push(bill)
+      }
+    }
+    return allBills
+  }
+
+  handleRoundTrip(foundTrip) {
+    this.log('info', 'handleRoundTrip starts')
+    let allTrips = []
+    // We're using 2 as the loop limit because you only get 2 trips per roundTrip
+    for (let i = 0; i < 2; i++) {
+      const trip = foundTrip.trip
+      const journeyValue = i === 0 ? 'outwardJourney' : 'inwardJourney'
+      // tripProofDetails could be empty and tripProofOperation could be present or not. If it's present, there's not necesarily a pdf to download.
+      const hasPdf = Boolean(
+        trip.tripDetails.tripProofDetails[i]?.tripProofOperation?.url
+      )
+      if (!hasPdf) {
+        allTrips.push(null)
+        continue
+      }
+
+      const vendorRef = trip.id
+      const departureDate = new Date(
+        trip.tripDetails[journeyValue].departureDate
+      )
+      const arrivalDate = new Date(trip.tripDetails[journeyValue].arrivalDate)
+      const originStation = trip.tripDetails[journeyValue].originLabel
+      const destinationStation = trip.tripDetails[journeyValue].destinationLabel
+      const tripDuration = trip.tripDetails[journeyValue].duration
+      const tripReference = trip.tripDetails[journeyValue].references[0]
+      // There an invisible character between the price and the currency.
+      // To avoid any problems, we replace this char with it's "visible" version
+      const amountAndCurrency = trip.tripDetails.tripProofDetails[
+        i
+      ].priceLabel.replace(/\s/g, ' ')
+      const [amount, currency] = amountAndCurrency.split(' ')
+      const totalAmount = trip.tripDetails[journeyValue].priceLabel
+        .replace(/\s/g, ' ')
+        .split(' ')[0]
+      const fileurl =
+        trip.tripDetails.tripProofDetails[i].tripProofOperation.url
+
+      const filename = `${format(
+        departureDate,
+        'yyyy-MM-dd'
+      )}_SNCF_RoundTrip_${originStation}-${destinationStation}_${amount}${currency}.pdf`
+
+      const oneBill = {
+        vendor: 'sncf-connect.com',
+        // We didn't dispose of a purchase date so we're using the departureDate
+        date: departureDate,
+        amount: parseInt(amount),
+        currency,
+        vendorRef,
+        filename,
+        fileurl,
+        trip: {
+          departureDate,
+          arrivalDate,
+          originStation,
+          destinationStation,
+          tripDuration,
+          tripReference,
+          totalAmount
+        },
+        fileAttributes: {
+          metadata: {
+            contentAuthor: 'SNCF',
+            datetime: departureDate,
+            datetimeLabel: 'issueDate',
+            carbonCopy: true
+          }
+        }
+      }
+      allTrips.push(oneBill)
+    }
+    return allTrips
+  }
+
+  handleSingleTrip(foundTrip) {
+    this.log('info', 'handleSingleTrip starts')
+    const trip = foundTrip.trip
+    // tripProofOperation could be present or not. If it's present, there's not necesarily a pdf to download.
+    const hasPdf = Boolean(
+      trip.tripDetails.outwardJourney.tripProof.tripProofOperation?.url
+    )
+    if (!hasPdf) {
+      return null
+    }
+
+    const vendorRef = trip.id
+    const departureDate = new Date(
+      trip.tripDetails.outwardJourney.departureDate
+    )
+    const arrivalDate = new Date(trip.tripDetails.outwardJourney.arrivalDate)
+    const originStation = trip.originLabel
+    const destinationStation = trip.destinationLabel
+    const tripDuration = trip.duration
+    const tripReference = trip.tripDetails.outwardJourney.references[0]
+    // There an invisible character between the price and the currency.
+    // To avoid any problems, we replace this char with it's "visible" version
+    const amountAndCurrency =
+      trip.tripDetails.outwardJourney.priceLabel.replace(/\s/g, ' ')
+    const [amount, currency] = amountAndCurrency.split(' ')
+    const fileurl =
+      trip.tripDetails.outwardJourney.tripProof.tripProofOperation.url
+
+    const filename = `${format(
+      departureDate,
+      'yyyy-MM-dd'
+    )}_SNCF_OneWay_${originStation}-${destinationStation}_${amount}${currency}.pdf`
+
+    const oneBill = {
+      vendor: 'sncf-connect.com',
+      // We didn't dispose of a purchase date so we're using the departureDate
+      date: departureDate,
+      amount: parseInt(amount),
+      currency,
+      vendorRef,
+      filename,
+      fileurl,
+      trip: {
+        departureDate,
+        arrivalDate,
+        originStation,
+        destinationStation,
+        tripDuration,
+        tripReference
+      },
+      fileAttributes: {
+        metadata: {
+          contentAuthor: 'SNCF',
+          datetime: departureDate,
+          datetimeLabel: 'issueDate',
+          carbonCopy: true
+        }
+      }
+    }
+    return oneBill
+  }
 }
 
 const connector = new SncfContentScript()
@@ -250,7 +480,9 @@ connector
       'waitForOtpCode',
       'clickPreLoginButton',
       'findAndClickInfosButton',
-      'getIdentity'
+      'getIdentity',
+      'getBills',
+      'waitForInterception'
     ]
   })
   .catch(err => {
